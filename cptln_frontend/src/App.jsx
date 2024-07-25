c2bd278c13cf0580acc7d1e1377f6bf11f8d5204
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-07-25 08:35:46 -0500

    Tailwind configuration

--- cptln_frontend/src/App.jsx
+++ cptln_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-700 hover:text-green-950">
+    <h1 className="text-3xl font-bold underline text-logo_color_2-600 hover:text-green-950">
       Hello world!
     </h1>
   )
--- cptln_frontend/tailwind.config.js
+++ cptln_frontend/tailwind.config.js
@@ -4,7 +4,26 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        logo_color_1: {
+          DEFAULT: "#CD612D",
+          600: "#A25F3B",
+        },
+        logo_color_2: {
+          DEFAULT: "#47797A",
+          600: "#3C5050",
+        },
+        logo_color_3: {
+          DEFAULT: "#9D1A2E",
+          600: "#742732",
+        },
+        logo_color_4: {
+          DEFAULT: "#BBB237",
+          600: "#908A42",
+        },
+      },
+    },
   },
   plugins: [],
 }
