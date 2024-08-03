735ea21dce767d26d8f7fac2c71179fcadbd4a51
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-08-03 14:10:24 -0500

    change

--- cptln_frontend/src/pages/client/pages/AboutUs/index.jsx
+++ cptln_frontend/src/pages/client/pages/AboutUs/index.jsx
@@ -22,7 +22,7 @@ export const AboutUs = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolasdbasbdksbdore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exerciasdsdtation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            aliquip ex ea commodo consequat. Dusfis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
